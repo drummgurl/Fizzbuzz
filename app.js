3a37d90e21f2aa4137949aed9e15e0760970a193
drummgurl <drummgurl@Sara-Talbots-MacBook-Pro.local>
2014-11-05 20:54:06 -0500

    changes1

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ $(document).ready(function() {
 	});
 
 	$('input').keyup(function(e){
-        if(e.which == 13) {//Enter key pressed
+        if(e.which === 13) {//Enter key pressed
         event.preventDefault();
         post('#results');
   		}
 
     for (var i = 1; i <= 100; i++){
         if (i % 15 === 0) {
-            console.log('FizzBuzz'); 
+            console.log("FizzBuzz"); 
             } else if (i % 3 === 0){
-            console.log('Fizz');
+            console.log("Fizz");
             } else if (i % 5 === 0){
-            console.log('Buzz');
+            console.log("Buzz");
             } else {
             console.log(i);
             }
