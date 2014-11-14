1b88d7aa2bf58b407963fb20e91f5216c9d2d2c3
drummgurl <drummgurl@Sara-Talbots-MacBook-Pro.local>
2014-11-13 20:58:09 -0500

    Changes

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
-	$('form').on('submit', function(event) {
-		event.preventDefault();
-	});
-
-	$('input').keyup(function(e){
-        if(e.which === 13) {//Enter key pressed
+    $('form').on('submit', function(event) {
         event.preventDefault();
-        post('#results');
-  		}
+    });
 
-    for (var i = 1; i <= 100; i++){
-        if (i % 15 === 0) {
-            console.log("FizzBuzz"); 
-            } else if (i % 3 === 0){
-            console.log("Fizz");
-            } else if (i % 5 === 0){
-            console.log("Buzz");
-            } else {
-            console.log(i);
-            }
-        }
-    }
-}
+    $('input').keyup(function(event){
+        if(event.which == 13) { //Enter key pressed
+            event.preventDefault();
+            
+            var userNumber = parseInt($("input").val());
+
+            for ( var i = userNumber; i <= 100; i++){
+                if (i % 15 === 0) {
+                    console.log('FizzBuzz'); 
+                } else if (i % 3 === 0){
+                    console.log('Fizz');
+                } else if (i % 5 === 0){
+                    console.log('Buzz');
+                } else {
+                    console.log(i);
+                }
+            } // closes for loop
+        } // closes if at top
+
+    }); // Closes input keyup handler
+});  // Closes document ready
